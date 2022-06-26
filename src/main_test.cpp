0bea1e8307df1061e758ac69b2639dd77b2ce0a8
lllllll221 <xzkov221@proton.me>
2022-06-26 12:29:11 +0300

    need to add relevance and draw a conclusion

--- src/main_test.cpp
+++ src/main_test.cpp
@@ -7,12 +7,23 @@
 
 const int characters = 341 /*all symbols*/ - (21 * 4) /*words*/;
 const std::set<int> numbers = {30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230};
+std::set<std::string> keys_set;
+std::vector<std::string> keys_vector;
+std::string key;
+
+std::string ReadLine() {
+    std::string user;
+    getline(std::cin, user);
+    return user;
+}
 
 std::string WordChoice(const std::vector<std::string>& words) {
     std::random_device rd;   // недетерминированный генератор
     std::mt19937 gen(rd());  // посеять мерсенн твистер
     std::uniform_int_distribution<> word(0, words.size() - 1); // распределять результаты между 0 и всеми эелемнтами вектора
     std::string say = words[word(gen)];
+    keys_set.insert(say);
+    keys_vector.push_back(say);
     return say;
 }
 
@@ -40,17 +51,41 @@ std::vector<char> Convert(const std::vector<std::string>& output) {
     return converted;
 }
 
+void ChoosingTheRightWord(const std::vector<std::string>& keys_vector) {
+    std::random_device rd;   // недетерминированный генератор
+    std::mt19937 gen(rd());  // посеять мерсенн твистер
+    std::uniform_int_distribution<> k(0, keys_vector.size() - 1);
+    key =  keys_vector[k(gen)];
+}
+
+/*
+int RelevanceCheck(const std::string& user, const std::string& key) {
+
+}
+*/
+
 int main() {
     const std::vector<std::string> output = GeneratingOutput(punctuation_string, words);
-    const std::vector<char> converted = Convert(output); 
-    for (size_t i = 0; i < addresses.size(); i++) {
-        std::cout << addresses[i] << " ";
-        for (int j = 1; j <= 10; j++) {
-            std::cout << converted[j + (10 * i)];
+    const std::vector<char> converted = Convert(output);
+    std::string user;
+    for (int i = 0; i < 4; i++) {
+        for (size_t i = 0; i < addresses.size(); i++) {
+            std::cout << addresses[i] << " ";
+            for (int j = 1; j <= 10; j++) {
+                std::cout << converted[j + (10 * i)];
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+        user = ReadLine();
+        if (user == key) {
+            std::cout << "Win!" << std::endl;
+            break;
+        }
+        std::cout << "-----------------" << std::endl;
+        std::cout << "----NICE  TRY----" << std::endl;
+        std::cout << "-----------------" << std::endl;
     }
-    
+/*
     std::cout << "-----------------" << std::endl;
     std::cout << "Output vector - ";
 
@@ -58,4 +93,5 @@ int main() {
         std::cout << c;
     }
     std::cout << std::endl;
+*/
 }
